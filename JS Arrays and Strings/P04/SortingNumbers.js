c847f49c92556b716b2a47f9f45b9e711e952823
nbzhk <rumenovnikolay@gmail.com>
2024-11-01 14:50:06 +0100

    exercises

---
+++ JS Arrays and Strings/P04/SortingNumbers.js
@@ -0,0 +1,26 @@
+function sortingNumbers(arr) {
+    const sorted = arr.sort((a,b) => a - b);
+
+    const result = [];
+
+    let i = 0;
+    let j = sorted.length - 1;
+
+    while(i <= j) {
+        if(i === j) {
+            result.push(sorted[i]);
+        } else {
+            result.push(sorted[i]);
+            result.push(sorted[j]);
+        }
+
+        i++;
+        j--;
+    }
+
+
+    console.log(result)
+    return result;
+}
+
+sortingNumbers([1, 65, 3, 52, 48, 63, 31, 18, 56]);
