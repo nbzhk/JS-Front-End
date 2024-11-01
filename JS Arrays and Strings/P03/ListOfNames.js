59feb2e72654266b6bc39255b06127f862817aed
nbzhk <rumenovnikolay@gmail.com>
2024-11-01 14:25:59 +0100

    exercises

---
+++ JS Arrays and Strings/P03/ListOfNames.js
@@ -0,0 +1,13 @@
+function listOfNames(arr) {
+    const sorted = arr.sort((a,b) => a.localeCompare(b));
+
+    for (let i = 0; i < sorted.length; i++) {
+        console.log(`${i + 1}.${sorted[i]}`)        
+    }
+
+}
+
+listOfNames(["John",
+    "Bob",
+    "Christina",
+    "Ema"]);
