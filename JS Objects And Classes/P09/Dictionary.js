df7602778de6d3a1034d2468c9467217a0d3bec4
nbzhk <rumenovnikolay@gmail.com>
2024-11-16 01:13:39 +0100

    exercises

---
+++ JS Objects And Classes/P09/Dictionary.js
@@ -0,0 +1,28 @@
+function makeADictionary(input) {
+
+    const dictionary = {};
+
+    input.forEach(command => {
+
+        const obj = JSON.parse(command);
+
+        const[word, definition] = Object.entries(obj)[0];
+
+        dictionary[word] = definition;
+    });
+
+    Object.entries(dictionary)
+        .sort()
+        .forEach(([word, definition]) => {
+            console.log(`Term: ${word} => Definition: ${definition}`);
+        })
+
+}
+
+makeADictionary([
+    '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+    '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+    '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+    '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+    '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}'
+]);
