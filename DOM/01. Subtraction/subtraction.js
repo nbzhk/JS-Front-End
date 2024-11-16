e1050923cb709c562e4d6e2904dd6ff0aa83939a
nbzhk <rumenovnikolay@gmail.com>
2024-11-16 01:29:43 +0100

    exercises

--- DOM/01. Subtraction/subtraction.js
+++ DOM/01. Subtraction/subtraction.js
@@ -1,3 +1,7 @@
 function subtract() {
-    //TODO
+    let firstNumber = document.getElementById('firstNumber').value;
+    let secondNumber = document.getElementById('secondNumber').value;
+
+
+    document.getElementById('result').innerHTML = (firstNumber - secondNumber).toString();
 }
