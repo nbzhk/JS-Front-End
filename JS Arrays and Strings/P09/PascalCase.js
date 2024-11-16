38adecb65363e36127ca63779bd3bb2b57d36b43
nbzhk <rumenovnikolay@gmail.com>
2024-11-16 01:20:21 +0100

    exercises

---
+++ JS Arrays and Strings/P09/PascalCase.js
@@ -0,0 +1,14 @@
+function pascalCase(string) {
+
+    const regex = /[A-Z][a-z]*/g;
+
+    let wordsArr = string.match(regex);
+
+    
+
+    console.log(wordsArr.join(', '))
+
+}
+
+pascalCase('SplitMeIfYouCanHaHaYouCantOrYouCan');
+pascalCase('HoldTheDoor');
