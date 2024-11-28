d03004aa9138929d88f30dabbfc11691db60b965
Nikolay <rumenovnikolay@gmail.com>
2024-11-28 23:13:41 +0100

    exercises

--- DOM/06. Format the Text/format-the-text.js
+++ DOM/06. Format the Text/format-the-text.js
@@ -1,3 +1,23 @@
 function solve() {
-  // TODO
+    const inputElement = document.querySelector('#input');
+    const outputElement = document.querySelector('#output');
+
+    const sentences = inputElement.value.split('. ');
+    const sentencesPerParagraph = 3;
+
+    const numberOfParagraphs = Math.ceil(sentences.length / sentencesPerParagraph);
+
+    let output = '';
+
+    for (let i = 0; i < numberOfParagraphs; i++) {
+        const start = i * sentencesPerParagraph;
+        const end = start + sentencesPerParagraph;
+
+        const inputSentence = sentences.slice(start, end).join('.');
+
+        output += `<p>${inputSentence}</p>\n`;
+    }
+
+    outputElement.innerHTML = output;
+
 }
