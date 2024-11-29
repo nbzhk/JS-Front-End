9dc2dd15670f2dde6c6c4ef7defd73d2795cdd75
Nikolay <rumenovnikolay@gmail.com>
2024-11-29 18:05:43 +0100

    exercises

--- DOM-Introduction/09. Number Convertor/number-convertor.js
+++ DOM-Introduction/09. Number Convertor/number-convertor.js
@@ -1,3 +1,12 @@
 function solve() {
-    // TODO
+
+    const input = Number(document.querySelector('#input').value);
+    const type = document.querySelector("#selectMenuTo").value;
+    const resultElement = document.querySelector('#result');
+
+    if (type === 'binary') {
+        resultElement.value = input.toString(2);
+    } else if (type === 'hexadecimal') {
+        resultElement.value = input.toString(16).toUpperCase();
+    }
 }
