ea2aa555b349e35b80a4cbd1ac61efd2eebf578b
Nikolay <rumenovnikolay@gmail.com>
2024-11-29 17:51:39 +0100

    exercises

--- DOM-Introduction/08. Generate Report/generate-report.js
+++ DOM-Introduction/08. Generate Report/generate-report.js
@@ -1,3 +1,23 @@
 function solve() {
-    //TODO
+    const tableHeadRow = document.querySelector('table thead tr');
+    const tableBodyRows = document.querySelectorAll('table tbody tr');
+
+    const checkedElements = [...tableHeadRow.children]
+        .map((th, i) => ( { el: th.children[0], name: th.children[0].getAttribute('name'), index: i  } ))
+        .filter(obj => obj.el.checked);
+
+    console.log(checkedElements);
+
+    const outputData = [...tableBodyRows]
+        .map(row =>
+            checkedElements.reduce((acc, curr) => {
+                acc[curr.name] = row.children[curr.index].textContent;
+                return acc;
+            }, {})
+        );
+
+    console.log(outputData);
+
+    const outputEl = document.querySelector('#output');
+    outputEl.value = JSON.stringify(outputData);
 }
