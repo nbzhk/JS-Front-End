d3151c51ba24f903b77cde401bc706c80c9f6d04
Nikolay <rumenovnikolay@gmail.com>
2024-11-30 15:52:06 +0100

    exercises

--- DOM-Events/01. Sections/sections.js
+++ DOM-Events/01. Sections/sections.js
@@ -1,5 +1,22 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-   // TODO
+    const input = document.querySelector('input[type="text"]').value.split(', ');
+    const contentEl = document.getElementById('content');
+
+
+    input.forEach((el) => {
+        const pElement = document.createElement('p');
+        const divElement = document.createElement('div');
+
+        pElement.textContent = el;
+        pElement.style.display = 'none';
+
+        divElement.appendChild(pElement);
+        divElement.addEventListener('click', (event) => {
+            event.target.querySelector('p').style.display = 'block';
+        });
+
+        contentEl.appendChild(divElement);
+    });
 }
