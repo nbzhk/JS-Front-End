f7e769fddf8cca857f97debc88523658ec319e19
Nikolay <rumenovnikolay@gmail.com>
2024-11-30 21:23:46 +0100

    exercises

--- DOM-Events/04. Fill-Dropdown/fill-dropdown.js
+++ DOM-Events/04. Fill-Dropdown/fill-dropdown.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const formEl = document.querySelector('form');
+    const menuEl = document.getElementById('menu');
+
+
+    formEl.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const newTextEl = document.getElementById('newItemText');
+        const newValueEl = document.getElementById('newItemValue');
+
+        const newText = newTextEl.value;
+        const newValue = newValueEl.value;
+
+        const newOptElement = document.createElement('option');
+        newOptElement.setAttribute('value', newValue);
+        newOptElement.textContent = newText;
+
+        menuEl.appendChild(newOptElement);
+
+        newTextEl.value = '';
+        newValueEl.value = '';
+
+    });
 }
