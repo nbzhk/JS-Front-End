0453458ad9450acedce9704f2f017c2d92725e83
Nikolay <rumenovnikolay@gmail.com>
2024-11-30 21:03:50 +0100

    exercises

--- DOM-Events/03. Locked-Profile/locked-profile.js
+++ DOM-Events/03. Locked-Profile/locked-profile.js
@@ -1,5 +1,36 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const profileElements = document.querySelectorAll('.profile');
+
+    profileElements.forEach(profile => {
+        const buttonsElement = profile.querySelector('button');
+        buttonsElement.addEventListener('click', handleClick);
+    });
+
+    function handleClick(event) {
+        const currentProfileElement = event.target.closest('.profile');
+        const state = currentProfileElement
+            .querySelector('input[name*="Locked"]:checked')
+            .getAttribute('id');
+
+
+        if (state.includes('Unlock')) {
+            const hiddenFieldsEl = currentProfileElement.querySelector('.hidden-fields');
+            changeView(hiddenFieldsEl);
+            changeButton(currentProfileElement);
+        }
+    }
+
+    function changeView(hiddenFieldsEl) {
+        const currentView = window.getComputedStyle(hiddenFieldsEl).display;
+        hiddenFieldsEl.style.display = currentView === 'none' ? 'block' : 'none';
+    }
+
+    function changeButton(currentProfileElement) {
+        const button = currentProfileElement.querySelector('button');
+        const currentValue = button.textContent;
+
+        button.textContent = currentValue === 'Show more' ? 'Show less' : 'Show more';
+    }
 }
