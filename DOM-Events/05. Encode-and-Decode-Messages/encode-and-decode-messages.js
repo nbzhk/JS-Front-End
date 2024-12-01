1731aa34d1cc41e7ee3af0ef16ef5072f68d0a75
Nikolay Bozhkov <rumenovnikolay@gmail.com>
2024-12-01 17:05:10 +0000

    exercises

--- DOM-Events/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
+++ DOM-Events/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
@@ -1,5 +1,28 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const encodeFormEl = document.querySelector('#encode');
+    const decodeFormEl = document.querySelector('#decode');
+
+    encodeFormEl.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const textEl = e.target.querySelector('textarea');
+        const message = textEl.value;
+        const encodedMessage = message.split('')
+            .map(char => String.fromCharCode(char.charCodeAt(0) + 1));
+
+        decodeFormEl.querySelector('textarea').value = encodedMessage.join('');
+        textEl.value = '';
+    });
+
+    decodeFormEl.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const decodedMessage = decodeFormEl.querySelector('textarea').value;
+        const encodedMessage = decodedMessage.split('')
+            .map(char => String.fromCharCode(char.charCodeAt(0) - 1));
+
+        e.target.querySelector('textarea').value = encodedMessage.join('');
+    });
 }
