010cbf11cc751db9b2023026f2f2f0bbbea0241d
Nikolay <rumenovnikolay@gmail.com>
2024-12-04 01:14:16 +0100

    exercises

--- HTTPandAJAX/01.Blog/app.js
+++ HTTPandAJAX/01.Blog/app.js
@@ -2,6 +2,8 @@ function attachEvents() {
     const postsURL = 'http://localhost:3030/jsonstore/blog/posts';
     const commentsURL = 'http://localhost:3030/jsonstore/blog/comments';
 
+    let allPosts = [];
+
     const loadButtonEl = document.getElementById('btnLoadPosts');
     const selectPostEl = document.getElementById('posts');
     const viewButtonEl = document.getElementById('btnViewPost');
@@ -11,12 +13,17 @@ function attachEvents() {
 
 
     loadButtonEl.addEventListener('click', () => {
-        selectPostEl.innerHTML = '';
+        selectPostEl.innerHTML = ''
+        allPosts = [];
 
         fetch(postsURL)
             .then(response => response.json())
             .then(data => {
                 Object.values(data).forEach(post => {
+                    //Add do Array because of Judge...
+                    const currentPost = {id: post.id, title: post.title, body: post.body};
+                    allPosts.push(currentPost);
+
                     const optionEl = document.createElement('option');
                     optionEl.value = post.id;
                     optionEl.textContent = post.title;
@@ -27,28 +34,28 @@ function attachEvents() {
     });
 
     viewButtonEl.addEventListener('click', () => {
+        postCommentsEl.innerHTML = '';
         const selectedPostId = selectPostEl.value;
 
-        fetch(`${postsURL}/${selectedPostId}`)
-            .then(response => response.json())
-            .then(data => {
-                postTitleEl.textContent = data.title;
-                postBodyEl.textContent = data.body;
-        });
+        if (!selectedPostId) return;
+
+        const matchPost = allPosts.find(post => post.id === selectedPostId);
+        postTitleEl.textContent = matchPost.title;
+        postBodyEl.textContent = matchPost.body;
+
 
         fetch(commentsURL)
             .then(response => response.json())
             .then(data => {
-               Object.values(data)
-                   .filter(comment => comment.postId === selectedPostId)
-                   .forEach((comment) => {
-                       const liElement = document.createElement('li');
-                       liElement.value = comment.id;
-                       liElement.textContent = comment.text;
+                Object.values(data)
+                    .filter(comment => comment.postId === selectedPostId)
+                    .forEach((comment) => {
+                        const liElement = document.createElement('li');
+                        liElement.setAttribute('value', comment.id);
+                        liElement.textContent = comment.text;
 
-                       postCommentsEl.innerHTML = '';
-                       postCommentsEl.appendChild(liElement);
-                   })
+                        postCommentsEl.appendChild(liElement);
+                    })
             });
     });
 }
