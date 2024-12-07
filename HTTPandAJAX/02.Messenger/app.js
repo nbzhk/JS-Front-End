400d6c47a67d1fc21346354687ea0d3a3af2a9a9
Nikolay <rumenovnikolay@gmail.com>
2024-12-07 20:45:01 +0100

    exercises

--- HTTPandAJAX/02.Messenger/app.js
+++ HTTPandAJAX/02.Messenger/app.js
@@ -1,5 +1,36 @@
 function attachEvents() {
-    // TODO:
+    const BASE_URL = 'http://localhost:3030/jsonstore/messenger';
+
+    const sendBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+    const messagesEl = document.getElementById('messages');
+
+    sendBtn.addEventListener('click', () => {
+        const [authorEl, messageEl] = document.querySelectorAll('input[type=text]');
+
+       fetch(BASE_URL, {
+           method: 'POST',
+           body: JSON.stringify({
+               author: authorEl.value,
+               content: messageEl.value,
+           }),
+       }).then(response => response.json())
+        .then(data => console.log(data))
+    });
+
+    refreshBtn.addEventListener('click', () => {
+
+        fetch(BASE_URL)
+            .then(response => response.json())
+            .then(data => displayMessages(data))
+    });
+
+    function displayMessages(data) {
+        Object.values(data).forEach((msg) => {
+            console.log(msg);
+            messagesEl.textContent += `${msg.author}: ${msg.content}\n`;
+        });
+    }
 }
 
 attachEvents();
