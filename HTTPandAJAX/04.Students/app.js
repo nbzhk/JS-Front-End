219832c12a8768a0960ae8d9b149bb35b2563eed
Nikolay <rumenovnikolay@gmail.com>
2024-12-08 13:49:10 +0100

    exercises

--- HTTPandAJAX/04.Students/app.js
+++ HTTPandAJAX/04.Students/app.js
@@ -1,5 +1,81 @@
 function attachEvents() {
-  // TODO:
+    const BASE_URL = 'http://localhost:3030/jsonstore/collections/students';
+
+    const submitBtnEl = document.getElementById('submit');
+    const tableEL = document.querySelector('#results tbody');
+
+    const firstNameEl = document.querySelector('input[name="firstName"]');
+    const lastNameEl = document.querySelector('input[name="lastName"]');
+    const facultyNumberEl = document.querySelector('input[name="facultyNumber"]');
+    const gradeEl = document.querySelector('input[name="grade"]');
+
+    const students = [];
+
+    submitBtnEl.addEventListener('click', createStudent)
+
+    function createStudent() {
+        if (firstNameEl.value && lastNameEl.value && facultyNumberEl.value && gradeEl.value) {
+            let firstName = firstNameEl.value.trim();
+            let lastName = lastNameEl.value.trim();
+            let facultyNumber = facultyNumberEl.value.trim();
+            let grade = gradeEl.value.trim();
+
+            const newStudent = {
+                firstName: firstName,
+                lastName: lastName,
+                facultyNumber: facultyNumber,
+                grade: grade,
+            }
+            console.log(newStudent);
+            students.push(newStudent);
+            console.log(students);
+
+
+            firstNameEl.value = '';
+            lastNameEl.value = '';
+            facultyNumberEl.value = '';
+            gradeEl.value = '';
+        }
+    }
+
+    submitBtnEl.addEventListener('click', extractStudents);
+
+    function extractStudents() {
+        fetch(BASE_URL)
+            .then(response => response.json())
+            .then(data => {
+                Object.values(data).forEach((student) => {
+                    students.push(student);
+                });
+            })
+    }
+
+    submitBtnEl.addEventListener('click', load)
+
+    function load() {
+        students.forEach(student => {
+            const rowElement = document.createElement('tr');
+            const firstNameCell = document.createElement('td');
+            const lastNameCell = document.createElement('td');
+            const facultyCell = document.createElement('td');
+            const gradeCell = document.createElement('td');
+
+            firstNameCell.textContent = student.firstName;
+            lastNameCell.textContent = student.lastName;
+            facultyCell.textContent = student.facultyNumber;
+            gradeCell.textContent = student.grade;
+
+            rowElement.appendChild(firstNameCell);
+            rowElement.appendChild(lastNameCell);
+            rowElement.appendChild(facultyCell);
+            rowElement.appendChild(gradeCell);
+
+            console.log(rowElement)
+
+            tableEL.appendChild(rowElement);
+        })
+    }
+
 }
 
 attachEvents();
